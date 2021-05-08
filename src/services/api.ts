95df5315afd63ad947345a9e60600b5210d3590f
Tiago100h <tiagogiuliatte.p@gmail.com>
2021-05-08 12:53:08 -0300

    Correção id do episódio

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Tiago100h/podcastr-fakeapi/'
+  baseURL: 'https://my-json-server.typicode.com/Tiago100h/podcastr-fakeapi'
 })
